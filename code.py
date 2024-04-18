79080bea5235aabe3f9ceafd0822199cc7dd7073
BataschJr <adnanworks24@gmail.com>
2024-04-18 16:05:04 +0530

    added the code of bow & tfidf

--- code.py
+++ code.py
@@ -26,11 +26,11 @@ from sklearn.metrics import classification_report,confusion_matrix,accuracy_scor
 
 # import os
 # print(os.listdir("../input"))
-# import warnings
-# warnings.filterwarnings('ignore')
+import warnings
+warnings.filterwarnings('ignore')
 
 
-imdb_data=pd.read_csv('D:/Uni/T9/COMP347/Final Project/nlp_project/IMDB Dataset.csv')
+imdb_data=pd.read_csv('./imdb_dataset.csv')
 print(imdb_data.shape)
 imdb_data.head(10)
 
@@ -55,3 +55,121 @@ print(test_reviews.shape,test_sentiments.shape)
 tokenizer=ToktokTokenizer()
 # setting English stopwords
 stopword_list=nltk.corpus.stopwords.words('english')
+
+#Removing the html strips
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+#Removing the square brackets
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+#Removing the noisy text
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    return text
+#Apply function on review column
+imdb_data['review']=imdb_data['review'].apply(denoise_text)
+
+#Define function for removing special characters
+def remove_special_characters(text, remove_digits=True):
+    pattern=r'[^a-zA-z0-9\s]'
+    text=re.sub(pattern,'',text)
+    return text
+#Apply function on review column
+imdb_data['review']=imdb_data['review'].apply(remove_special_characters)
+
+def simple_stemmer(text):
+    ps=nltk.porter.PorterStemmer()
+    text= ' '.join([ps.stem(word) for word in text.split()])
+    return text
+#Apply function on review column
+imdb_data['review']=imdb_data['review'].apply(simple_stemmer)
+
+stop=set(stopwords.words('english'))
+print("stop: ",stop)
+
+#removing the stopwords
+def remove_stopwords(text, is_lower_case=False):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
+#Apply function on review column
+imdb_data['review']=imdb_data['review'].apply(remove_stopwords)
+
+#normalized train reviews
+norm_train_reviews=imdb_data.review[:40000]
+norm_train_reviews[0]
+
+#Normalized test reviews
+norm_test_reviews=imdb_data.review[40000:]
+norm_test_reviews[45005]
+
+
+# Bags of words model
+# It is used to convert text documents to numerical vectors or bag of words.
+cv=CountVectorizer(min_df=0.0,max_df=1.0,binary=False,ngram_range=(1,3))
+#transformed train reviews
+cv_train_reviews=cv.fit_transform(norm_train_reviews)
+#transformed test reviews
+cv_test_reviews=cv.transform(norm_test_reviews)
+
+print('BOW_cv_train:',cv_train_reviews.shape)
+print('BOW_cv_test:',cv_test_reviews.shape)
+
+
+# Term Frequency-Inverse Document Frequency model (TFIDF)
+# It is used to convert text documents to matrix of tfidf features.
+tv=TfidfVectorizer(min_df=0.0,max_df=1.0,use_idf=True,ngram_range=(1,3))
+#transformed train reviews
+tv_train_reviews=tv.fit_transform(norm_train_reviews)
+#transformed test reviews
+tv_test_reviews=tv.transform(norm_test_reviews)
+print('Tfidf_train:',tv_train_reviews.shape)
+print('Tfidf_test:',tv_test_reviews.shape)
+
+#labeling the sentient data
+lb=LabelBinarizer()
+#transformed sentiment data
+sentiment_data=lb.fit_transform(imdb_data['sentiment'])
+print(sentiment_data.shape)
+
+#Spliting the sentiment data
+train_sentiments=sentiment_data[:40000]
+test_sentiments=sentiment_data[40000:]
+print(train_sentiments)
+print(test_sentiments)
+
+# Modelling the dataset
+#training the model
+lr=LogisticRegression(penalty='l2',max_iter=500,C=1,random_state=42)
+#Fitting the model for Bag of words
+lr_bow=lr.fit(cv_train_reviews,train_sentiments)
+print(lr_bow)
+#Fitting the model for tfidf features
+lr_tfidf=lr.fit(tv_train_reviews,train_sentiments)
+print(lr_tfidf)
+
+#Predicting the model for bag of words
+lr_bow_predict=lr.predict(cv_test_reviews)
+print(lr_bow_predict)
+##Predicting the model for tfidf features
+lr_tfidf_predict=lr.predict(tv_test_reviews)
+print(lr_tfidf_predict)
+
+
+# Accuracy of the model
+#Accuracy score for bag of words
+lr_bow_score=accuracy_score(test_sentiments,lr_bow_predict)
+print("lr_bow_score :",lr_bow_score)
+#Accuracy score for tfidf features
+lr_tfidf_score=accuracy_score(test_sentiments,lr_tfidf_predict)
+print("lr_tfidf_score :",lr_tfidf_score)
+
